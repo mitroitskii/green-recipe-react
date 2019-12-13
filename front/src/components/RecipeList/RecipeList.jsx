9258db399fd4dc5aab1b0a91565d959a6b0f6a0f
lemishevsky <lemishevsky@gmail.com>
2019-12-13 14:25:31 +0300

    0.1 update names

--- front/src/components/RecipeList/RecipeList.jsx
+++ front/src/components/RecipeList/RecipeList.jsx
@@ -2,8 +2,32 @@ import React from 'react';
 import RecipeItem from '../RecipeItem';
 
 
-const RecipeList = (recipes) => {
-  const elements = recipes.map((item) => {
+const RecipeList = () => {
+
+  const eda = [
+    {
+      id: '12423542342',
+      name: 'Пельмешки',
+      image: 'https://img1.russianfood.com/dycontent/images_upl/53/sm_52955.jpg',
+      instructions: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc eu semper risus. Aenean tempus metus sed dolor condimentum varius. Pellentesque mi lacus, consequat ac suscipit non, sagittis eget nisi. Pellentesque consequat egestas purus, at sodales elit sollicitudin a. Nam vel leo diam. Morbi in orci vel leo egestas ultricies non.',
+      priceTotal: '250 руб',
+      caloriesTotal: '450 ккал',
+      ingredients: [],
+      author: {},
+    },
+    {
+      id: '312412351345234',
+      name: 'Гречка с мясом',
+      image: 'https://img1.russianfood.com/dycontent/images_upl/376/sm_375300.jpg',
+      instructions: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus at posuere sem. In nulla tortor, semper eget dui quis, tristique iaculis mauris. Nunc eu vestibulum justo, eu volutpat arcu. Nullam in nisl non diam molestie porttitor vel in ex. Quisque viverra purus non ante commodo, quis fringilla ligula dapibus. Suspendisse',
+      priceTotal: '150 руб',
+      caloriesTotal: '500 ккал',
+      ingredients: [],
+      author: {},
+    },
+  ]
+
+  const elements = eda.map((item) => {
     const { id } = item;
     return (
       <li key={id} className="list-group-item">
