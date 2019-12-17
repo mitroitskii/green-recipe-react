9695538612becfa1b15a17642f964cfe9ff9cb7c
lemishevsky <lemishevsky@gmail.com>
2019-12-17 20:01:29 +0300

    Recipe-list done without styles

--- front/src/components/RecipeItem/RecipeItem.jsx
+++ front/src/components/RecipeItem/RecipeItem.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Link } from 'react-router-dom'
 
 const RecipeItem = (item) => {
-  const { name, image, _id, ingredients, priceTotal, caloriesTotal } = item;
+  const { name, image, _id, ingredients, priceTotal, caloriesTotal, category, authorName, hours="00", minutes="00" } = item;
   // вернуть инструкции по желанию
   return (
     <div>
@@ -11,6 +11,9 @@ const RecipeItem = (item) => {
       <p>Количество ингредиентов: {ingredients.length}</p>
       <p>Стоимость рецепта: {priceTotal} руб. </p>
       <p>Калорийность: {caloriesTotal} ккал </p>
+      <p>Категория: {category}</p>
+      <p>Автор: {authorName}</p>
+      <p>Время приготовления: {hours}:{minutes} </p>
     </div>
   );
 }
