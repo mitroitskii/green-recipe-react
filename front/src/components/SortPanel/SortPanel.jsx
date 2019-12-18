9140a6b25f8fe16196fc872cfe2a1ea1d5a2cd2e
lemishevsky <lemishevsky@gmail.com>
2019-12-18 12:56:17 +0300

    sort-panel with grommet

--- front/src/components/SortPanel/SortPanel.jsx
+++ front/src/components/SortPanel/SortPanel.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { Box, Button, Text } from 'grommet';
 
 
 export default class SortPanel extends Component {
@@ -33,7 +34,7 @@ export default class SortPanel extends Component {
     switch (this.state.directionPrice) {
       case 'up': this.setState({ directionPrice: 'down', directionCal: '' });
         break;
-      case 'down': this.setState({ direcdirectionPricetionCal: 'up', directionCal: '' });
+      case 'down': this.setState({ directionPrice: 'up', directionCal: '' });
         break;
       default: this.setState({ directionPrice: 'up', directionCal: '' })
     }
@@ -41,18 +42,16 @@ export default class SortPanel extends Component {
 
   render() {
     const { recipesFromApiSortingByPrice, recipesFromApiSortingByCalories } = this.props;
-    const { directionCal, directionPrice, id } = this.state
+    const { directionCal, directionPrice, id } = this.state;
+    const priceButton = (directionPrice === 'up') ? 'По цене ▼' : (directionPrice === 'down') ? 'По цене ▲' : 'По цене';
+    const calorieButton = (directionCal === 'up') ? 'По калорийности ▼' : (directionCal === 'down') ? 'По калорийности ▲' : 'По калорийности';
     return (
-      <div>
-        <Link to={'/recipes/' + id}><button>Удиви меня!</button></Link>
-        <span>Сортировать:</span>
-        <button onClick={() => { recipesFromApiSortingByPrice(directionPrice); this.togglePrice() }}>
-          По цене {(directionPrice === 'up') && '▼'} {(directionPrice === 'down') && '▲'}
-        </button>
-        <button onClick={() => { recipesFromApiSortingByCalories(directionCal); this.toggleCal() }}>
-          По калорийности {(directionCal === 'up') && '▼'} {(directionCal === 'down') && '▲'}
-        </button>
-      </div >
+      <Box>
+        <Link to={'/recipes/' + id}><Button label='Удиви меня!' /></Link>
+        <Text>Сортировать:</Text>
+        <Button label={priceButton} onClick={() => { recipesFromApiSortingByPrice(directionPrice); this.togglePrice() }} />
+        <Button label={calorieButton} onClick={() => { recipesFromApiSortingByCalories(directionCal); this.toggleCal() }} />
+      </Box>
     );
   }
 }
