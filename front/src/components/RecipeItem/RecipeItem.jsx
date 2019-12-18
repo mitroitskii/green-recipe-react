e6eb1146f4f05f9bb186d24895ec86734595f16f
lemishevsky <lemishevsky@gmail.com>
2019-12-18 15:42:32 +0300

    sort-panel add grommit

--- front/src/components/RecipeItem/RecipeItem.jsx
+++ front/src/components/RecipeItem/RecipeItem.jsx
@@ -6,36 +6,38 @@ const RecipeItem = (item) => {
   const { name, image, _id, ingredients, priceTotal, caloriesTotal, category, authorName, hours = "00", minutes = "00" } = item;
   // вернуть инструкции по желанию
   return (
-    <Grid
-      margin='medium'
-      rows={['fit', 'small']}
-      columns={['small', 'medium']}
-      gap="small"
-      areas={[
-        { name: 'header', start: [0, 0], end: [1, 0] },
-        { name: 'img', start: [0, 1], end: [0, 1] },
-        { name: 'text', start: [1, 1], end: [1, 1] },
-      ]}
-    >
-      <Box gridArea='header' background='neutral-1'>
-        <Link to={'/recipes/' + _id}>{name}</Link>
-      </Box>
-      <Box gridArea='img'>
-        <Image src={image}
-          alt={name}
-          fit='contain'
-          alignSelf='start'
-        />
-      </Box>
-      <Box gridArea='text' background='ligth-2'>
-        <Text>Количество ингредиентов: {ingredients.length}</Text>
-        <Text>Стоимость рецепта: {priceTotal} руб. </Text>
-        <Text>Калорийность: {caloriesTotal} ккал </Text>
-        <Text>Категория: {category}</Text>
-        <Text>Автор: {authorName}</Text>
-        <Text>Время приготовления: {hours}:{minutes} </Text>
-      </Box>
-    </Grid>
+    <Link style={{ "textDecoration": "none" }} to={'/recipes/' + _id}>
+      <Grid
+        margin='medium'
+        rows={['fit', 'small']}
+        columns={['small', 'medium']}
+        gap="small"
+        areas={[
+          { name: 'header', start: [0, 0], end: [1, 0] },
+          { name: 'img', start: [0, 1], end: [0, 1] },
+          { name: 'text', start: [1, 1], end: [1, 1] },
+        ]}
+      >
+        <Box gridArea='header' background='neutral-1' round='xsmall'>
+          <Text margin='xsmall'>{name}</Text>
+        </Box>
+        <Box gridArea='img'>
+          <Image src={image}
+            alt={name}
+            fit='contain'
+            alignSelf='start'
+          />
+        </Box>
+        <Box gridArea='text' background='ligth-2'>
+          <Text>Количество ингредиентов: {ingredients.length}</Text>
+          <Text>Стоимость рецепта: {priceTotal} руб. </Text>
+          <Text>Калорийность: {caloriesTotal} ккал </Text>
+          <Text>Категория: {category}</Text>
+          <Text>Автор: {authorName}</Text>
+          <Text>Время приготовления: {hours}:{minutes} </Text>
+        </Box>
+      </Grid>
+    </Link>
   );
 }
 
--- front/src/components/SortPanel/SortPanel.jsx
+++ front/src/components/SortPanel/SortPanel.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import { Box, Button, Text } from 'grommet';
+import { Grid, Box, Button, Text } from 'grommet';
 
 
 export default class SortPanel extends Component {
@@ -46,12 +46,30 @@ export default class SortPanel extends Component {
     const priceButton = (directionPrice === 'up') ? 'По цене ▼' : (directionPrice === 'down') ? 'По цене ▲' : 'По цене';
     const calorieButton = (directionCal === 'up') ? 'По калорийности ▼' : (directionCal === 'down') ? 'По калорийности ▲' : 'По калорийности';
     return (
-      <Box>
-        <Link to={'/recipes/' + id}><Button label='Удиви меня!' /></Link>
-        <Text>Сортировать:</Text>
-        <Button label={priceButton} onClick={() => { recipesFromApiSortingByPrice(directionPrice); this.togglePrice() }} />
-        <Button label={calorieButton} onClick={() => { recipesFromApiSortingByCalories(directionCal); this.toggleCal() }} />
-      </Box>
+      <Grid
+        margin='xsmall'
+        rows={['fit']}
+        columns={['fit', 'fit', 'fit', 'fit']}
+        gap="small"
+        areas={[
+          { name: 'surprise', start: [0, 0], end: [1, 0] },
+          { name: 'sort', start: [1, 0], end: [2, 0] },
+          { name: 'sortButton1', start: [2, 0], end: [3, 0] },
+          { name: 'sortButton2', start: [3, 0], end: [4, 0] },
+        ]}>
+        <Box gridArea='surprise'>
+          <Link to={'/recipes/' + id}><Button label='Удиви меня!' /></Link>
+        </Box>
+        <Box gridArea='sort'>
+          <Text margin='xsmall'>Сортировать:</Text>
+        </Box>
+        <Box gridArea='sortButton1'>
+          <Button label={priceButton} onClick={() => { recipesFromApiSortingByPrice(directionPrice); this.togglePrice() }} />
+        </Box>
+        <Box gridArea='sortButton2'>
+          <Button label={calorieButton} onClick={() => { recipesFromApiSortingByCalories(directionCal); this.toggleCal() }} />
+        </Box>
+      </Grid>
     );
   }
 }
