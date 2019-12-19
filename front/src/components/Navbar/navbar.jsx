57fc741f00a56a67b31529e6aaca1848248d3609
Dmitriy Troitskiy <mitrafantos@gmail.com>
2019-12-19 13:12:50 +0300

    added navbar links

--- front/src/components/Navbar/navbar.jsx
+++ front/src/components/Navbar/navbar.jsx
@@ -15,24 +15,31 @@ class Navbar extends React.Component {
       <Header width="80%" align="center" justify="center" gap="medium">
         {/* <Link to='/'>HOME</Link> */}
         <Box align="center" justify="center">
-          <Anchor as={Link} to="/" label="Домашняя страница" />
+          <Anchor as={Link} to="/" label="Главная" />
         </Box>
-        {
-          this.props.isLoggedIn
-            ? (<Box direction="row" align="center" justify="center" >
-              <Anchor label="Выйти" as={Link} to="/login" onClick={this.logout.bind(this)} />
-            </Box>
-            )
-            : (
-              <Box direction='row' justify="center" align="center" gap="medium">
-                <Anchor label="Войти" as={Link} to="/login" />
-                <Anchor label="Регистрация" as={Link} to="/registration" />
-              </Box>
-            )
-        }
-
+        {this.props.isLoggedIn ? (
+          <Box direction="row" align="center" justify="center" gap="medium">
+            <Anchor
+              label="Мои рецепты"
+              as={Link}
+              to={`/users/${this.props.userId}`}
+            />
+            <Anchor label="Создать рецепт" as={Link} to="/recipes/new" />
+            <Anchor
+              label="Выйти"
+              as={Link}
+              to="/login"
+              onClick={this.logout.bind(this)}
+            />
+          </Box>
+        ) : (
+          <Box direction="row" justify="center" align="center" gap="medium">
+            <Anchor label="Войти" as={Link} to="/login" />
+            <Anchor label="Регистрация" as={Link} to="/registration" />
+          </Box>
+        )}
       </Header>
-    )
+    );
   }
 }
 function mapStateToProps(store) {
@@ -52,7 +59,7 @@ function mapDispatchToProps(dispatch) {
 export default connect(mapStateToProps, mapDispatchToProps)(Navbar);
 
 // const response = await fetch("http://localhost:5000/api/logout", {method: 'DELETE',
-                          // credentials: "include"})
+// credentials: "include"})
 // if (response.status === 200) {
 //   let result = await response.json()
 //   let isLoggedIn = result.isLoggedIn
