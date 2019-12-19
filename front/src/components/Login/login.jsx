9593d5e9920218839def35d5a179a3c9e3644d54
Dmitriy Troitskiy <mitrafantos@gmail.com>
2019-12-19 19:30:18 +0300

    fixed errors

--- front/src/components/Login/login.jsx
+++ front/src/components/Login/login.jsx
@@ -3,9 +3,15 @@ import React from 'react';
 import { Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { loginFetchAC, clearStatusAC } from '../../redux/actions/actions';
-import Preloader from "../Preloader/preloader"
-import { Box, Text, Button,Heading } from 'grommet';
-import { TextInputField, Form, PasswordInputField, validators } from 'grommet-controls';
+import Preloader from '../Preloader/preloader';
+import { Box, Text, Button, Heading } from 'grommet';
+import {
+  TextInputField,
+  Form,
+  PasswordInputField,
+  validators
+} from 'grommet-controls';
+
 class Login extends React.Component {
   constructor(props) {
     super(props);
@@ -13,7 +19,7 @@ class Login extends React.Component {
       username: '',
       password: ''
     };
-  
+
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
@@ -24,66 +30,64 @@ class Login extends React.Component {
   async handleSubmit(values) {
     this.props.loginFetch({
       username: values.username,
-      password: values.password,
+      password: values.password
     });
   }
 
   render() {
     return (
-      <Box 
-      width="30%" 
-      margin="large" 
-      pad="large" 
-      border={{ "color": "border" }} 
-      background="light-1" 
+      <Box
+        width="30%"
+        margin="large"
+        pad="large"
+        border={{ color: 'border' }}
+        background="light-1"
       >
-      <Heading level="2" 
-      // margin={{
-      //   "top":"none",
-      //   "bottom":"small"
-      //   }} >Вход</Heading>
+        <Heading
+          level="2"
+          margin={{
+            top: 'none',
+            bottom: 'small'
+          }}
+        >
+          Вход
+        </Heading>
         <Form
-          basis='medium'
+          basis="medium"
           focusFirstChild={false}
           {...this.props}
           onSubmit={this.handleSubmit}
         >
-          <TextInputField label='Имя' name='username' />
+          <TextInputField label="Имя" name="username" />
           <PasswordInputField
-            label={(
-              <Box direction='row' align='center' justify='between'>Пароль</Box>
-            )}
-            description='Password'
-            name='password'
-            validation={
-              [
-              validators.required(), 
-              validators.minLength(5),
-              validators.alphaNumeric()
-            ]
+            label={
+              <Box direction="row" align="center" justify="between">
+                Пароль
+              </Box>
             }
+            description="Password"
+            name="password"
+            validation={[validators.required()]}
           />
-          <Box pad={{ vertical: 'medium' }} align='end'>
-            <Button hoverIndicator='background' 
-            primary={true} type='submit' 
-            // alignSelf="center" 
-            label='Войти' 
+          <Box pad={{ vertical: 'medium' }} align="end">
+            <Button
+              hoverIndicator="background"
+              primary
+              type="submit"
+              // alignSelf="center"
+              label="Войти"
             />
           </Box>
-          <Box direction='row' 
-          alignSelf='center' 
-          gap='small' 
-          align='center'>
-          </Box>
+          <Box direction="row" alignSelf="center" gap="small" align="center" />
         </Form>
         <Box>
-          {
-            this.props.loadingFetch
-              ? <Preloader/>
-              : this.props.isLoggedIn ? (
-                <Redirect to="/" />)
-                : <Text>{this.props.logRegstatusError}</Text>
-          }
+          {this.props.loadingFetch ? (
+            <Preloader />
+          ) : this.props.isLoggedIn ? (
+            <Redirect to="/" />
+          ) : (
+            <Text>{this.props.logRegstatusError}</Text>
+          )}
         </Box>
       </Box>
     );
@@ -97,7 +101,6 @@ function mapStateToProps(store) {
     logRegstatusError: store.logRegstatusError
   };
 }
-
 
 function mapDispatchToProps(dispatch) {
   return {
@@ -146,13 +149,11 @@ export default connect(mapStateToProps, mapDispatchToProps)(Login);
 //   </form>
 //   <div>
 //     {
-//     this.props.loadingFetch 
-//     ? (<span className={'statustext'}>loading</span>) 
+//     this.props.loadingFetch
+//     ? (<span className={'statustext'}>loading</span>)
 //     : this.props.isLoggedIn ? (
-//       <Redirect to="/" />) 
+//       <Redirect to="/" />)
 //       : (<span className={'statustext'}>{this.props.logRegstatusError}</span>)
 //       }
 //   </div>
 // </div>
-
-
