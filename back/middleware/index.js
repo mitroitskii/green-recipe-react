b3713e45e2e71687304bcb8a1f3fbf1700a8b87c
Dmitriy Troitskiy <mitrafantos@gmail.com>
2019-12-24 15:23:20 +0300

    Fixed CORS for Heroku

--- back/middleware/index.js
+++ back/middleware/index.js
@@ -15,7 +15,7 @@ module.exports = function(app) {
   app.use(express.json());
 
   app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.header('Access-Control-Allow-Origin', '*');
     res.header(
       'Access-Control-Allow-Headers',
       'Origin, X-Requested-With, Content-Type, Accept',
